8e7b4d142c27f90f27662eb7f2461ab1f125ffd4
Trond Bråthen <tabrath@gmail.com>
2016-12-30 20:37:42 +0100

    initial

--- LibP2P.Protocol.Tests/Properties/AssemblyInfo.cs
+++ LibP2P.Protocol.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibP2P.Protocol")]
-[assembly: AssemblyDescription("libp2p protocol")]
+[assembly: AssemblyTitle("LibP2P.Protocol.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tabrath")]
-[assembly: AssemblyProduct("LibP2P.Protocol")]
-[assembly: AssemblyCopyright("Copyright © tabrath 2016")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LibP2P.Protocol.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ce416346-7955-4118-8d71-44d9077bce36")]
+[assembly: Guid("1d9f2111-1c19-43ee-9aab-6afaee7523b2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
